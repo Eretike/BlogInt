5b3c174dfbd9541fe941fa6a2a47a49c71472d43
OlivierLB <lebloas.olivier@gmail.com>
2016-08-17 18:15:46 +0200

    Modif assets
    Pour rectifier l'erreur de placement du assets

---
+++ application/views/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>BlogInt</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/css-index.css" /> 
+</head>
+<body>
+	<section id="test">			
+		coucou
+	</section>
+
+</body>
+</html>
