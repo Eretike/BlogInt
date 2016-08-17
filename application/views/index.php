7e2ac573ffd5e70f0381a7b9723c1cb62037dce2
OlivierLB <lebloas.olivier@gmail.com>
2016-08-17 22:11:48 +0200

    Création page d'accueil
    Premiere esquisse de la page d'accueil

--- application/views/index.php
+++ application/views/index.php
@@ -5,10 +5,33 @@
 	<title>BlogInt</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/css-index.css" /> 
 </head>
-<body>
-	<section id="test">			
-		coucou
-	</section>
+<body id="cssBody">
+	<div id="news">			
+		<section class="titre">LES MEILLEURS</section>
+		<div class="trou">
+			ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule 
+		</div>
+		<section class="titre">LES MEILLEURS DU JOUR</section>
+		<div class="trou">
+			ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule 
+		</div>
+		<section class="titre">LES MEILLEURS DU MOIS</section>
+		<div class="trou">
+			ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule 
+		</div>
+	</div>
+	<div id="best">
+		<section class="titre">LES NOUVEAUX VENUS</section>
+		<section id="case1">
+			fff
+		</section>
+		<section id="case2">
+			fff
+		</section>
+		<section id="case3">
+			fff
+		</section>
+	</div>
 
 </body>
 </html>
