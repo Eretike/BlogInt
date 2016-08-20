9f77f5a97003fa0e5066503d5065c52243599f36
OlivierLB <lebloas.olivier@gmail.com>
2016-08-20 03:59:25 +0200

    Grosse modif accueil + header
    Tout est dans le titre

--- application/views/header.php
+++ application/views/header.php
@@ -3,5 +3,15 @@
 <head lang="en">
 	<meta charset="UTF-8">
 	<title>BlogInt</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/css-header.css" /> 
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/css-index.css" /> 
 </head>
+<body id="cssBody">
+	<ul>
+		<li><a href="#home" class="active">Publier</a></li>
+	 	<li><a href="#home">Home</a></li>
+		<li><a href="#news">News</a></li>
+		<li><a href="#contact">Contact</a></li>
+		<li style="float:right"><a href="#about">Se connecter</a></li>
+		<li style="float:right"><a href="#about">S'inscrire</a></li>
+	</ul>
--- application/views/index.php
+++ application/views/index.php
@@ -1,29 +1,34 @@
-<body id="cssBody">
-	<div id="news">			
-		<section class="titre">LES MEILLEURS</section>
-		<div class="trou">
-			ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule 
+	<div id="fondpage">
+		<div id="news">			
+			<section class="titre">JOUR</section>
+			<div class="trou">
+				Procedente igitur mox tempore cum adventicium nihil inveniretur, relicta ora maritima in Lycaoniam adnexam Isauriae se contulerunt ibique densis intersaepientes itinera praetenturis provincialium et viatorum opibus pascebantur.
+			</div>
+			<section class="titre">MOIS</section>
+			<div class="trou">
+				Oportunum est, ut arbitror, explanare nunc causam, quae ad exitium praecipitem Aginatium inpulit iam inde a priscis maioribus nobilem, ut locuta est pertinacior fama. nec enim super hoc ulla documentorum rata est fides.
+			</div>
+			<section class="titre">GÉNÉRAL</section>
+			<div class="trou">
+				Nunc vero inanes flatus quorundam vile esse quicquid extra urbis pomerium nascitur aestimant praeter orbos et caelibes, nec credi potest qua obsequiorum diversitate coluntur homines sine liberis Romae.
+			</div>
 		</div>
-		<section class="titre">LES MEILLEURS DU JOUR</section>
-		<div class="trou">
-			ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule 
-		</div>
-		<section class="titre">LES MEILLEURS DU MOIS</section>
-		<div class="trou">
-			ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule ta gueule 
+		<div id="best">
+			<section id="case1">
+				<div class="transparence">
+					Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam pellerentur ab urbe praecipites, sectatoribus disciplinarum liberalium inpendio paucis sine respiratione ulla extrusis, tenerentur minimarum adseclae veri, quique id simularunt ad tempus, et tria milia saltatricum ne interpellata quidem cum choris totidemque remanerent magistris.
+				</div>
+			</section>
+			<section id="case2">
+				<div class="transparence">
+					Ut enim quisque sibi plurimum confidit et ut quisque maxime virtute et sapientia sic munitus est, ut nullo egeat suaque omnia in se ipso posita iudicet, ita in amicitiis expetendis colendisque maxime excellit. Quid enim? Africanus indigens mei? Minime hercule! ac ne ego quidem illius; sed ego admiratione quadam virtutis eius, ille vicissim opinione fortasse non nulla, quam de meis moribus habebat, me dilexit; auxit benevolentiam consuetudo. Sed quamquam utilitates multae et magnae consecutae sunt, non sunt tamen ab earum spe causae diligendi profectae.
+				</div>
+			</section>
+			<section id="case3">
+				<div class="transparence">
+					Eius populus ab incunabulis primis ad usque pueritiae tempus extremum, quod annis circumcluditur fere trecentis, circummurana pertulit bella, deinde aetatem ingressus adultam post multiplices bellorum aerumnas Alpes transcendit et fretum, in iuvenem erectus et virum ex omni plaga quam orbis ambit inmensus, reportavit laureas et triumphos, iamque vergens in senium et nomine solo aliquotiens vincens ad tranquilliora vitae discessit.
+				</div>
+			</section>
 		</div>
 	</div>
-	<div id="best">
-		<section class="titre">LES NOUVEAUX VENUS</section>
-		<section id="case1">
-			fff
-		</section>
-		<section id="case2">
-			fff
-		</section>
-		<section id="case3">
-			fff
-		</section>
-	</div>
-
 </body>
